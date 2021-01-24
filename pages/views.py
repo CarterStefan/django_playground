a2779890d681a9eed494c2d6d6051ba5d9607cf6
CarterStefan <ssttefancarter@gmail.com>
2021-01-24 13:19:15 +0000

    Deployed to Heroku

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
